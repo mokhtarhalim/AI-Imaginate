67214cdf7ab26201edd0379cdc41d601f02aba1d
unknown <halimkh0403@gmail.com>
2023-04-26 23:41:49 +0100

    Creating MongoDb database

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from "./mongodb/connect";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req, res) => {
     res.send('Hello From halim');
 })
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server started on 8080'))
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server started on 8080'))
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 startServer();
