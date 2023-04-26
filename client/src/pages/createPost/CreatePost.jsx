9a93b453543fa4124843f5a98b05620df3e2f510
unknown <halimkh0403@gmail.com>
2023-04-26 22:23:50 +0100

    Create FormFields Design

--- client/src/pages/createPost/CreatePost.jsx
+++ client/src/pages/createPost/CreatePost.jsx
@@ -7,7 +7,7 @@ import FormField from "../../components/FormField"
 import Loader from "../../components/Loader"
 
 export default function CreatePost() {
-  
+
   const navigate = useNavigate();
   const [form, setForm] = useState({
     name: '',
@@ -16,11 +16,50 @@ export default function CreatePost() {
   });
 
   const [generatingImg, setGeneratingImg] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSupriseMe = () => {
+    
+  }
 
   return (
-    <div>
-      
-    </div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and visually stunning images through AI Imaginate and share them with the Community</p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="an oil painting by Matisse of a humanoid robot playing chess"
+            value={form.promt}
+            handleChange={handleChange}
+            isSupriseMe
+            handleSupriseMe={handleSupriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
- 
