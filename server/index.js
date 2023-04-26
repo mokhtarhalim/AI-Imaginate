aecbb558ab0b3141963b6537942fead14b862a7d
unknown <halimkh0403@gmail.com>
2023-04-27 00:04:04 +0100

    Start generating OpenAi Data

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import imaginateRoutes from "./routes/imaginateRoutes.js"
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/imaginate', imaginateRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello From halim');
